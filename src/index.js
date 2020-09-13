4d5f11883b3ea292987f12f77d513a4def95ed6a
Renato Santos <renato_jfs@hotmail.com>
2020-09-13 00:04:46 -0300

    Insomnia Routes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,43 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
 app.get('/projects', (request, response) => {
-  return response.json({ 
-  });
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 2',
+    'Projeto 3',
+  ]);
 });
 
 app.listen(3333, () => {
