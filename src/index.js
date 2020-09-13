c3d7105b3882cefb71b78958dff97eb79359792b
Renato Santos <renato_jfs@hotmail.com>
2020-09-12 23:18:23 -0300

    Clear

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World'})
+app.get('/projects', (request, response) => {
+  return response.json({ 
+  });
 });
 
 app.listen(3333, () => {
-  console.log('🚀 Back-end started!')
+  console.log('🚀 Back-end started!');
 });
