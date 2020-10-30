772ae7d5647c4512bbf0e5135185b6853ce11afc
Renato Santos <renato_jfs@hotmail.com>
2020-10-30 00:03:40 -0300

    Finish midware

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
-//const { response } = require('express');
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
-/**
- * GET: Buscar informações do back-end
- * POST: Criar uma informação no back-end
- * PUT/PATCH: Alterar uma informação no back-end
- *      PUT:     QUANDO VAI ATUALIZAR TODO OS DADOS AO MESMO TEMPO
- *      PATCH:   QUANDO VAI ATULIZAR DADOS ESPECIFICOS
- * DELETE: Deletar uma informação no back-end
- */
-
-/**
- * Tipos de parâmetros:
- * 
- * Query Params: Filtros e paginação
- * Routes Params: Identificar recursos (Atualizar/Deletar)
- * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
- */
-
-/**
- * Middleware:
- * 
- *   Interceptador de requisições que podem interrompter
- * totalmente a requisição ou alterar dados da requisição.
- */
 const projects = [];
+
+function logRequests (request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next(); //Proximo middleware
+
+  console.timeEnd(logLabel);
+}
+
+function validadeProjectId (request, response, next) {
+  const { id } = request.params
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.'})
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validadeProjectId);
 
 app.get('/projects', (request, response) => {
   const { title } = request.query
@@ -89,3 +90,27 @@ app.delete('/projects/:id', (request, response) => {
 app.listen(3333, () => {
   console.log('🚀 Back-end started!');
 });
+
+/**
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ *      PUT:     QUANDO VAI ATUALIZAR TODO OS DADOS AO MESMO TEMPO
+ *      PATCH:   QUANDO VAI ATULIZAR DADOS ESPECIFICOS
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Filtros e paginação
+ * Routes Params: Identificar recursos (Atualizar/Deletar)
+ * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
+ */
+
+/**
+ * Middleware:
+ * 
+ *   Interceptador de requisições que podem interrompter
+ * totalmente a requisição ou alterar dados da requisição.
+ */
